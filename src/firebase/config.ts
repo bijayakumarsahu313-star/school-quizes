c6932da0270ff7bbd2ebe1d7c112909950a3ae95
Bijaya kumar Sahu <bijayakumarsahu313@gmail.com>
2026-01-30 16:38:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,10 @@
-
 // The value of `firebaseConfig` is provided by the server and obtained in
 // `src/firebase/index.ts`. It should not be committed to version control.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+};
